7b47258fa545df814b66e50e9b03121769f942e8
Benjamin Westburg <38194972+westbenj2020@users.noreply.github.com>
2021-03-29 20:39:39 -0400

    03_29_2021
    Note: An attempt to separate a received message on the side of the second server is proving troublesome; I need to separate the concatenated components of the message as well as decrypt those segments with their appropriate keys. In doing so, I will become closer to extracting the ultimate timestamp which is required to authorize the client's attempted access to the final server. As before, the fifth and six steps of the authentication process require attention in wrapping up this project.,

--- client.py
+++ client.py
@@ -10,7 +10,8 @@ import hmac
 
 HOST = '127.0.0.1'  # The server socket's hostname or IP address
 PORT = 65432        # The port used by the server socket
-BLOCK_SIZE = 32
+PORT1 = 80
+BLOCK_SIZE = 64
 client_id = "CIS3319USERID"
 a_tg_server_id = "CIS3319TGSID"
 std_server_id = "CIS3319SERVERID"
@@ -21,72 +22,89 @@ lifetime4 = 86400
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # references created server socket
     s.connect((HOST, PORT))  # connects to server socket
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
-        s1.connect((HOST, PORT))
-        with open('clientKey.txt') as key:
-            hmacFile = open('hmacKey.txt')
-            hmacKey = hmacFile.read(6)
-            std_serverKeyFile = open('std_serverKey.txt')
-            std_serverKey = std_serverKeyFile.read(6)
-            a_tg_serverKeyFile = open('a_tg_serverKey.txt')
-            a_tg_serverKey = a_tg_serverKeyFile.read(6)
-            string = key.read(8)
-            string1 = string
-            #  cipher = DES.new(string.encode('utf-8'), DES.MODE_ECB)
-            plaintext1 = client_id + a_tg_server_id + timestamp1.__str__()  # retrieves plaintext input from user
-            plaintext1Encoded = plaintext1.encode('utf-8')
-            #  msgAuth = hmac.new(hmacKey, plaintext1, hashlib.sha256)
-            #  stringer = msgAuth.hexdigest()
-            #  msgWithHMAC = stringer + plaintext1.decode()
-            #  msgWithHMACEncoded = msgWithHMAC.encode('utf-8')
-            #  msg = cipher.encrypt(pad(msgWithHMACEncoded, BLOCK_SIZE))  # encrypts plaintext
-            s.sendall(plaintext1Encoded)  # sends encrypted message to server
-            print('plain message is: {}'.format(plaintext1))
-            cipher1 = DES.new(string1.encode('utf-8'), DES.MODE_ECB)
-            data = s.recv(64)  # receives data from server socket - 1024 byte limit
-            decryptedData = cipher1.decrypt(data)  # stores decoded byte data as string
-            unpad(decryptedData, BLOCK_SIZE)
-            decodedData = decryptedData.decode('utf-8').strip()
-            print('\n******************')
-            print('received ciphertext is: {}'.format(data.decode('utf-8', 'ignore')))  # prints decoded byte data
-            print('received plaintext is: {}'.format(decodedData))
-            print('******************')
-            #  extract tgs ticket to send back to tgs server
-            ticketTGS = {decodedData.replace(string1, '').replace(a_tg_serverKey, '').replace(a_tg_server_id, '')
-                         .replace(lifetime2.__str__(), '')}
-            timestamp2 = ''
-            for char in ticketTGS:
-                if char.isdigit():
-                    timestamp2.__add__(char)
-            timestamp3 = time.time()
-            authenticatorContents = client_id + networkAddress + timestamp3.__str__()
-            authenticatorContentsEncoded = authenticatorContents.encode('utf-8')
-            masterKeyCandTGS = string1 + a_tg_serverKey
-            cipher2 = DES.new(masterKeyCandTGS.encode('utf-8'), DES.MODE_ECB)
-            clientAuthenticator = cipher2.encrypt(pad(authenticatorContentsEncoded, BLOCK_SIZE))
-            messageForTGS = std_server_id + ticketTGS.__str__() + clientAuthenticator.__str__()
-            messageForTGSEncoded = messageForTGS.encode('utf-8')
-            s.sendall(messageForTGSEncoded)  # doesn't need to be encrypted
-            cipher3 = DES.new(masterKeyCandTGS.encode('utf-8'), DES.MODE_ECB)
-            data1 = s.recv(64)
-            decryptedData1 = cipher3.decrypt(data1)
-            unpad(decryptedData1, BLOCK_SIZE)
-            decodedData1 = decryptedData1.decode('utf-8').strip()
-            ticketV = {decodedData1.replace(string1, '').replace(std_serverKey, '').replace(std_server_id, '')}
-            timestamp4 = ''
-            for char in ticketV:
-                if char.isdigit():
-                    timestamp4.__add__(char)
-            #  whats left of ticketV is ticketV.
-            #  create second client authenticator
-            timestamp5 = time.time()
-            secondAuthenticatorContents = string1 + std_serverKey + client_id + networkAddress + timestamp5.__str__()
-            secondAuthenticatorContentsEncoded = secondAuthenticatorContents.encode('utf-8')
-            masterKeyCandV = string1 + std_serverKey
-            cipher4 = DES.new(masterKeyCandV.encode('utf-8'), DES.MODE_ECB)
-            secondClientAuthenticator = cipher4.encrypt(pad(secondAuthenticatorContentsEncoded, BLOCK_SIZE))
-            clientTostdServerMessage = ticketV.__str__() + secondClientAuthenticator.__str__()
-            #  now send to std server
+    with open('clientKey.txt') as key:
+        hmacFile = open('hmacKey.txt')
+        hmacKey = hmacFile.read(8)
+        std_serverKeyFile = open('std_serverKey.txt')
+        std_serverKey = std_serverKeyFile.read(8)
+        a_tg_serverKeyFile = open('a_tg_serverKey.txt')
+        a_tg_serverKey = a_tg_serverKeyFile.read(8)
+        string = key.read(8)
+        string1 = string
+        #  cipher = DES.new(string.encode('utf-8'), DES.MODE_ECB)
+        plaintext1 = client_id + a_tg_server_id + timestamp1.__str__()  # retrieves plaintext input from user
+        plaintext1Encoded = plaintext1.encode('utf-8')
+        #  msgAuth = hmac.new(hmacKey, plaintext1, hashlib.sha256)
+        #  stringer = msgAuth.hexdigest()
+        #  msgWithHMAC = stringer + plaintext1.decode()
+        #  msgWithHMACEncoded = msgWithHMAC.encode('utf-8')
+        #  msg = cipher.encrypt(pad(msgWithHMACEncoded, BLOCK_SIZE))  # encrypts plaintext
+        s.sendall(plaintext1Encoded)  # sends encrypted message to server
+        #  print('plain message is: {}'.format(plaintext1))
+        cipher1 = DES.new(string1.encode('utf-8'), DES.MODE_ECB)
+        data = s.recv(1024)  # receives data from server socket - 1024 byte limit
+        decryptedData = cipher1.decrypt(data)  # stores decoded byte data as string
+        unpaddedData = unpad(decryptedData, BLOCK_SIZE)
+        decodedData = unpaddedData.decode('utf-8')
+        ticketTGS = string1 + a_tg_serverKey + client_id + networkAddress + a_tg_server_id + decodedData[28:40]
+        print('\n******************')
+        #  print('received ciphertext is: {}'.format(data.decode('utf-8', 'ignore')))  # prints decoded byte data
+        print('received plaintext from authentication server: {}'.format(decodedData[0:40].strip()))
+        #  extract tgs ticket to send back to tgs server
+        print('received ticket from authentication server: {}'.format(ticketTGS).strip())
+        print('*******************')
+        timestamp3 = time.time().__trunc__()
+        authenticatorContents = client_id + networkAddress + timestamp3.__str__()
+        authenticatorContentsEncoded = authenticatorContents.encode('utf-8')
+        masterKeyCandTGS = string1[0:4] + a_tg_serverKey[-4:]
+        cipher2 = DES.new(masterKeyCandTGS.encode('utf-8'), DES.MODE_ECB)
+        clientAuthenticator = cipher2.encrypt(pad(authenticatorContentsEncoded, BLOCK_SIZE))
+        #  print(clientAuthenticator)
+        messageForTGS = std_server_id + ticketTGS.__str__() + clientAuthenticator.__str__()
+        messageForTGSEncoded = messageForTGS.encode('utf-8')
+        s.sendall(messageForTGSEncoded)  # doesn't need to be encrypted
+        cipher3 = DES.new(masterKeyCandTGS.encode('utf-8'), DES.MODE_ECB)
+        data1 = s.recv(1024)
+        decryptedData1 = cipher3.decrypt(data1)
+        unpaddedData1 = unpad(decryptedData1, BLOCK_SIZE)
+        decodedData1 = unpaddedData1.decode('utf-8').strip()
+        timestamp4 = decodedData1[31:41]
+        nonencryptedticketV = string1 + std_serverKey + client_id + networkAddress + std_server_id + \
+            timestamp4.__str__() + lifetime4.__str__()
+        nonencryptedticketVEncoded = nonencryptedticketV.encode('utf-8')
+        cipher2 = DES.new(std_serverKey.encode('utf-8'), DES.MODE_ECB)
+        encryptedticketV = cipher2.encrypt(pad(nonencryptedticketVEncoded, BLOCK_SIZE))
+        print('\nReceived plaintext from ticket-granting server: {}'.format(decodedData1))
+        print('\nReceived Ticket V from ticket-granting server: {}\n'.format(nonencryptedticketV))
+        key.close()
+        s.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+    s1.connect((HOST, PORT1))
+    #  whats left of ticketV is ticketV.
+    #  create second client authenticator
+    timestamp5 = time.time().__trunc__()
+    timestamp5PlusOne = timestamp5 + 1
+    secondAuthenticatorContents = string1 + std_serverKey + client_id + networkAddress + timestamp5.__str__()
+    secondAuthenticatorContentsEncoded = secondAuthenticatorContents.encode('utf-8')
+    masterKeyCandV = string1[0:4] + std_serverKey[-4:]
+    cipher4 = DES.new(masterKeyCandV.encode('utf-8'), DES.MODE_ECB)
+    secondClientAuthenticator = cipher4.encrypt(pad(secondAuthenticatorContentsEncoded, BLOCK_SIZE))
+    clientTostdServerMessage = encryptedticketV.__str__() + secondClientAuthenticator.__str__()
+    s1.sendall(clientTostdServerMessage.encode('utf-8'))
+    cipher5 = DES.new(masterKeyCandV.encode('utf-8'), DES.MODE_ECB)
+    data2 = s1.recv(1024)
+    decryptedData2 = cipher5.decrypt(data2)
+    unpad(decryptedData2, BLOCK_SIZE)
+    decodedData2 = decryptedData2.decode('utf-8').strip()
+    print('Received message from Server V: {}'.format(decodedData2))
+    if decodedData2 == timestamp5PlusOne:
+        print('Server V is validated.')
+    else:
+        print('Server V could not be validated.')
+
+
+
+
 
 
 
